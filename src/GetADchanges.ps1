1e98967337bf637614e6d1c70b86b7779eb931e8
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-08 15:27:48 +0200

    new

--- src/GetADchanges.ps1
+++ src/GetADchanges.ps1
@@ -1,2 +1,19 @@
-$result = Add-ADPrincipalGroupMembership -Identity $object -MemberOf $group -PassThru
-Write-Host "Successfully added '$object' to group '$group'"
+$DCs = Get-ADDomainController -Filter *  -server $server_name
+
+$startDate = (get-date).AddDays(-$days_past)
+
+foreach ($DC in $DCs){
+$events = Get-Eventlog -LogName Security -ComputerName $DC.Hostname -after $startDate | Where-Object {$_.eventID -eq 4728 -or $_.eventID -eq 4729}}
+
+ foreach ($e in $events){
+    
+
+    if (($e.EventID -eq 4728 )){
+        $result["ADchanges"] += "<li>"+$e.TimeGenerated+"--"+$e.ReplacementStrings[6]+ " made changes to '" +$e.ReplacementStrings[2]+ "' and added user '" + $e.ReplacementStrings[0] + "'</li>"
+  
+    }
+ 
+    if (($e.EventID -eq 4729 )) {    
+       $result["ADchanges"] += "<li>"+$e.TimeGenerated+"--"+$e.ReplacementStrings[6]+ " made changes to '" +$e.ReplacementStrings[2]+ "' and removed user '" + $e.ReplacementStrings[0] + "'</li>"
+    
+    }}    
